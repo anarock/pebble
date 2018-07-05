8e18d04e4a5594c59139e76ae459cd7e5cab0e35
ritz078 <rkritesh078@gmail.com>
2018-07-05 20:20:13 +0530

    chore(test): added script for test watch

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,9 @@
 import resolve from "rollup-plugin-node-resolve";
 import filesize from "rollup-plugin-filesize";
 import sourceMaps from "rollup-plugin-sourcemaps";
-import alias from "rollup-plugin-alias";
 import pkg from "./package.json";
-import path from "path";
 import babel from "rollup-plugin-babel";
 import commonjs from 'rollup-plugin-commonjs';
-
 
 const input = "compiled/index.js";
 const external = ["react"];
