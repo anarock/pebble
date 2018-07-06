022948c4ee42d9c4f803bd19ba8c5fee9dbea2e8
ritz078 <rkritesh078@gmail.com>
2018-07-06 18:31:24 +0530

    test(button): added an unit test

--- tests/button.test.tsx
+++ tests/button.test.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import renderer from "react-test-renderer";
 import Button from "@src/components/Button";
 import combos from "combos";
+import sinon from "sinon";
+import {mount} from "enzyme";
 
 describe("Button Combos test", () => {
 	const _props = {
@@ -19,6 +21,18 @@ describe("Button Combos test", () => {
 
 		const tree = button.toJSON();
 		expect(tree).toMatchSnapshot()
+	})
+});
+
+describe("Button: functionality", () => {
+	test("click is registered correctly", () => {
+		const fake = sinon.fake();
+
+		const button = mount(<Button onClick={fake}>Submit</Button>);
+
+		button.find("button").simulate("click");
+
+		expect(fake.calledOnce).toBeTruthy();
 	})
 });
 
