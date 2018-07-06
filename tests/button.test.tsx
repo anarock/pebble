a029bb4df2674c64e7dc75ed38541e1659f2953c
ritz078 <rkritesh078@gmail.com>
2018-07-06 18:40:13 +0530

    test(button): few fixes

--- tests/button.test.tsx
+++ tests/button.test.tsx
@@ -3,36 +3,39 @@ import renderer from "react-test-renderer";
 import Button from "@src/components/Button";
 import combos from "combos";
 import sinon from "sinon";
-import {mount} from "enzyme";
+import { mount } from "enzyme";
+
+function noop() {}
 
 describe("Button Combos test", () => {
-	const _props = {
-		disabled: [true, false],
-		type: ["primary", "secondary", "dropdown", "link"],
-		showRipple: [true, false]
-	};
+  const _props = {
+    disabled: [true, false],
+    type: ["primary", "secondary", "dropdown", "link"],
+    showRipple: [true, false]
+  };
 
-	const _combos = combos(_props);
+  const _combos = combos(_props);
 
-	test.each(_combos)("%o", (props) => {
-		const button = renderer.create(
-			<Button {...props} onClick={() => {}}>Submit</Button>
-		);
+  test.each(_combos)("%o", props => {
+    const button = renderer.create(
+      <Button {...props} onClick={noop}>
+        Submit
+      </Button>
+    );
 
-		const tree = button.toJSON();
-		expect(tree).toMatchSnapshot()
-	})
+    const tree = button.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
 
 describe("Button: functionality", () => {
-	test("click is registered correctly", () => {
-		const fake = sinon.fake();
+  test("click is registered correctly", () => {
+    const fake = sinon.fake();
 
-		const button = mount(<Button onClick={fake}>Submit</Button>);
+    const button = mount(<Button onClick={fake}>Submit</Button>);
 
-		button.find("button").simulate("click");
+    button.find("button").simulate("click");
 
-		expect(fake.calledOnce).toBeTruthy();
-	})
+    expect(fake.calledOnce).toBeTruthy();
+  });
 });
-
