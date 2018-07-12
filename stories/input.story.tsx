04dc9237399f52e1442743cd54f821a1b5397a6f
ritz078 <rkritesh078@gmail.com>
2018-07-12 18:19:39 +0530

    feat(storybook): added local state to components needing it

--- stories/input.story.tsx
+++ stories/input.story.tsx
@@ -2,9 +2,9 @@ import * as React from "react";
 import { storiesOf } from "@storybook/react";
 import Input from "../src/components/Input";
 import { boolean, select, text } from "@storybook/addon-knobs";
-import { action } from "@storybook/addon-actions";
 import { css } from "react-emotion";
 import DateInput from "../src/components/DateInput";
+import { withState } from "@dump247/storybook-state";
 
 const className = css({
   width: 400
@@ -13,23 +13,33 @@ const className = css({
 const type = ["text", "date", "password"];
 
 storiesOf("Input", module)
-  .add("Material", () => (
-    <Input
-      className={className}
-      onChange={action("change")}
-      type={select("type", type)}
-      placeholder={text("placeholder", "Name")}
-      fixLabelAtTop={boolean("fixLabelAtTop")}
-      required={boolean("required")}
-      value={text("value")}
-      readOnly={boolean("readOnly")}
-      disabled={boolean("disabled")}
-      message={text("message")}
-      errorMessage={text("errorMessage")}
-      successMessage={text("successMessage")}
-      textArea={boolean("textArea")}
-    />
-  ))
-  .add("Date", () => (
-    <DateInput placeholder="Date" onChange={action("change date")} />
-  ));
+  .add(
+    "Material",
+    withState({ value: "" })(({ store }) => (
+      <Input
+        className={className}
+        onChange={value => store.set({ value })}
+        type={select("type", type)}
+        placeholder={text("placeholder", "Name")}
+        fixLabelAtTop={boolean("fixLabelAtTop")}
+        required={boolean("required")}
+        value={store.state.value}
+        readOnly={boolean("readOnly")}
+        disabled={boolean("disabled")}
+        message={text("message")}
+        errorMessage={text("errorMessage")}
+        successMessage={text("successMessage")}
+        textArea={boolean("textArea")}
+      />
+    ))
+  )
+  .add(
+    "Date",
+    withState({ value: "" })(({ store }) => (
+      <DateInput
+        placeholder="Date"
+        value={store.state.value}
+        onChange={value => store.set({ value })}
+      />
+    ))
+  );
