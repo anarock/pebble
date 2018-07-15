4a03f2ab85b6692ba8a0f6b2c77255b1bf02e81b
ritz078 <rkritesh078@gmail.com>
2018-07-15 21:15:36 +0530

    fixes

--- src/theme/icons.ts
+++ src/theme/icons.ts
@@ -259,26 +259,5 @@ i:before {
 .icon-whatsapp:before {
   content: "\f13d";
 }
-
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  font-family: "Anarock", sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-
-.mb-10 {
-  margin-bottom: 10px;
-}
-
-.ml-10 {
-  margin-left: 10px;
-}
-
-.mt-10 {
-  margin-top: 10px;
-}
 `;
 }
