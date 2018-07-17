dddd09c9b2cff17fc6cd183d7ed907dc48911f89
Ritesh Kumar <ritz078@users.noreply.github.com>
2018-07-17 15:28:05 +0530

    Slider typing (#23)
    * fix(slider): add typings of rheostat as a dependency
    
    * refactor(styles): apply styling to whole website

--- src/theme/initStyles.ts
+++ src/theme/initStyles.ts
@@ -1,5 +1,17 @@
+import { injectGlobal } from "react-emotion";
 import { initIconStyles } from "./icons";
 
 export function initStyles() {
   initIconStyles();
+
+  injectGlobal`
+	* {	
+		margin: 0;	
+		padding: 0;	
+		box-sizing: border-box;	
+		font-family: "Anarock", sans-serif;	
+		-webkit-font-smoothing: antialiased;	
+		-moz-osx-font-smoothing: grayscale;	
+	}
+`;
 }
