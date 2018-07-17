91b400420506ae2c0a3b87059776c832790bb53c
Ritesh Kumar <ritz078@users.noreply.github.com>
2018-07-17 15:23:00 +0530

    build(unpkg): added support for unpkg cdn (#21)

--- scripts/generateIcon.js
+++ scripts/generateIcon.js
@@ -2,6 +2,29 @@ const webfontsGenerator = require("webfonts-generator");
 const path = require("path");
 const fs = require("fs");
 const prettier = require("prettier");
+const colors = require("colors");
+
+const anarockFonts = `
+@font-face{
+    font-family: "Anarock";
+    src: url(./fonts/anarock-regular.woff) format("woff"),
+         url(./fonts/anarock-regular.woff2) format("woff2"),
+         url(./fonts/anarock-regular.ttf) format("truetype"),
+         url(./fonts/anarock-regular.svg#Anarock) format("svg");
+    font-style: normal;
+    font-weight: normal;
+}
+
+@font-face{
+    font-family: "Anarock";
+    src: url(./fonts/anarock-medium.woff) format("woff"),
+         url(./fonts/anarock-medium.woff2) format("woff2"),
+         url(./fonts/anarock-medium.ttf) format("truetype"),
+         url(./fonts/anarock-medium.svg#Anarock) format("svg");
+    font-style: normal;
+    font-weight: bold;
+}
+`;
 
 fs.readdir(path.resolve(__dirname, "../svgs"), (err, data) => {
   if (err) {
@@ -17,12 +40,14 @@ fs.readdir(path.resolve(__dirname, "../svgs"), (err, data) => {
       fontName: "anarock-icons",
       types: ["woff2", "ttf", "eot", "woff"],
       css: false,
+      cssFontsUrl: "./fonts/",
       templateOptions: {
         baseSelector: "i"
       }
     },
     (err, result) => {
       const css = result.generateCss();
+
       const index = css.indexOf("i {");
 
       const fileContent = `
@@ -32,7 +57,7 @@ fs.readdir(path.resolve(__dirname, "../svgs"), (err, data) => {
           data.map(x => x.replace(".svg", ""))
         )}
         
-        export function initGlobalStyles() {
+        export function initIconStyles() {
           injectGlobal\`${prettier.format(css.substr(index), {
             parser: "css"
           })}\`
@@ -45,7 +70,23 @@ fs.readdir(path.resolve(__dirname, "../svgs"), (err, data) => {
           parser: "babylon"
         }),
         "utf-8",
-        console.log
+        err => {
+          if (err) console.log(err.message);
+          else console.log(colors.green.bold("icons.ts successfully created."));
+        }
+      );
+
+      fs.writeFile(
+        path.resolve(__dirname, "../src/theme/pebble.css"),
+        prettier.format(anarockFonts + css, {
+          parser: "css"
+        }),
+        "utf-8",
+        err => {
+          if (err) console.log(err.message);
+          else
+            console.log(colors.green.bold("pebble.css successfully created."));
+        }
       );
     }
   );
