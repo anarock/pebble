c5ad8eabaf18f32f6253b6118d5f6b90164594ac
Satwik Gupta <satwik.cpp@gmail.com>
2018-08-31 14:47:37 +0530

    display search-icon and className as props

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -3,8 +3,14 @@ import { cx } from "emotion";
 import { SearchProps } from "./typings/Search";
 import { searchStyle, searchWrapperStyle } from "./styles/Search.styles";
 
-const Search: React.SFC<SearchProps> = ({ type, onChange, placeholder }) => {
-  const wrapperClassName = cx(searchWrapperStyle, {
+const Search: React.SFC<SearchProps> = ({
+  type,
+  onChange,
+  placeholder,
+  showSearchIcon = true,
+  className
+}) => {
+  const wrapperClassName = cx(searchWrapperStyle, className, {
     __pebble__search__small: type === "small",
     __pebble__search__large: type === "large",
     __pebble__search__table: type === "table"
@@ -12,7 +18,7 @@ const Search: React.SFC<SearchProps> = ({ type, onChange, placeholder }) => {
 
   return (
     <div className={wrapperClassName}>
-      {type !== "large" && <i className="icon-search" />}
+      {type !== "large" && showSearchIcon && <i className="icon-search" />}
       <input
         className={searchStyle}
         type="text"
