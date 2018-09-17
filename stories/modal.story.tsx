f8b4b1d4502dc3c66140273e2319dc210568ea92
Chaitanya Gadodia <chaitanya.gadodia01@gmail.com>
2018-09-17 12:18:40 +0530

    test removed

--- stories/modal.story.tsx
+++ stories/modal.story.tsx
@@ -12,17 +12,8 @@ const style = css({
   backgroundColor: colors.white.base
 });
 
-let arr = [];
-for (let i = 0; i < 5000; i++) {
-  arr[i] = i;
-}
-
 storiesOf("Modal", module).add("simple", () => (
-  <Modal visible={boolean("Visible", true)}>
-    <div className={style}>
-      {arr.map(id => (
-        <div>{id}</div>
-      ))}
-    </div>
+  <Modal visible={boolean("Visible", false)}>
+    <div className={style} />
   </Modal>
 ));
