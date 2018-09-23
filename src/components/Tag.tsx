ad93445e28697b8478e445d0a39b1df6d03a666f
Rahul Yadav <dxtr026@users.noreply.github.com>
2018-09-23 20:39:20 +0530

    Update Tag.tsx

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -5,7 +5,7 @@ import { TagProps } from "./typings/Tag";
 import { cx } from "emotion";
 import Ink from "react-ink";
 
-const Tag: React.SFC<TagProps> = ({ label, color, onClose, class }) => {
+const Tag: React.SFC<TagProps> = ({ label, color, onClose, className }) => {
   const wrapperClassName = cx(tagStyle, {
     __pebble__tag__with__close: !!onClose
   });
