f60fa0074dff232e2dfed99e9ae8550e93578fcd
Aziz Khambati <aziz.h.khambati@gmail.com>
2018-10-09 18:12:12 +0530

    feat(phone-input): added Phone Number Input component (#105)

---
+++ src/components/styles/PhoneNumberInput.styles.ts
@@ -0,0 +1,10 @@
+import { css } from "emotion";
+
+export const wrapper = css({
+  display: "flex"
+});
+
+export const selectStyle = css({
+  width: "80px",
+  marginBottom: 0
+});
---
+++ src/components/typings/PhoneNumberInput.ts
@@ -0,0 +1,18 @@
+export interface Country {
+  id: number;
+  name: string;
+  country_code: string;
+}
+
+export interface PhoneNumberInputProps {
+  country_code: string;
+  phone: string;
+  onChange: (
+    { country_code, phone }: { country_code: string; phone: string }
+  ) => void;
+  countries: Country[];
+}
+
+export interface PhoneNumberInputState {
+  country?: Country;
+}
