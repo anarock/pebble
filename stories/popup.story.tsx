a4c5575247ad557b522de90d50ed7da1397918c3
Chaitanya Gadodia <chaitanya.gadodia01@gmail.com>
2018-10-24 16:42:59 +0530

    storybook action

--- stories/popup.story.tsx
+++ stories/popup.story.tsx
@@ -1,16 +1,13 @@
 import * as React from "react";
 import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
 import PopUp from "../src/components/PopUp";
 
 storiesOf("PopUp", module).add("simple", () => (
   <PopUp
     visible={true}
     headingText="Are you sure?"
-    onApprove={() => {
-      console.log("Approve");
-    }}
-    onClose={() => {
-      console.log("Close");
-    }}
+    onApprove={action("click")}
+    onClose={action("click")}
   />
 ));
