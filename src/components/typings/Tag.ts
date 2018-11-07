edbfa1f3174662b57df669d55a3269ee5d205f3c
Satwik Gupta <satwik.cpp@gmail.com>
2018-11-08 04:25:10 +0530

    tag's label can accept number (#140)
    * tag's label can accept number
    
    * test(sidebar): fix breaking tests.
    
    * feat(Toast): added support for passing className as prop.

--- src/components/typings/Tag.ts
+++ src/components/typings/Tag.ts
@@ -2,7 +2,7 @@ import { colors } from "../../theme";
 import { SetDifference } from "utility-types";
 
 export interface TagProps {
-  label: string;
+  label: string | number;
   color: SetDifference<keyof typeof colors, "white">;
   className?: string;
   onClose?: () => void;
--- src/components/typings/Toast.ts
+++ src/components/typings/Toast.ts
@@ -1,5 +1,9 @@
 export type ToastType = "success" | "error";
 
+export interface ToastProps {
+  className?: string;
+}
+
 export interface ToastState {
   type: ToastType;
   text: string;
