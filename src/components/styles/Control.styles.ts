cdd8e5ecc8d66a4e3072b42b93afdd6cda8b3ffa
Aziz Khambati <aziz.h.khambati@gmail.com>
2018-12-07 19:21:03 +0530

    Wrap Option Label and Left Align Radio

--- src/components/styles/Control.styles.ts
+++ src/components/styles/Control.styles.ts
@@ -1,26 +1,22 @@
 import { css } from "emotion";
 import { colors, typography } from "../../theme";
 
-export const controlStyle = css({
-  outline: "none",
-  position: "relative"
+export const radioIconStyle = css({
+  marginRight: 10,
+  fontSize: 16
 });
 
-export const controlContentStyle = css({
-  display: "flex",
-  justifyContent: "center",
-  ...typography.normal.regular,
+export const controlStyle = css({
   cursor: "pointer",
+  display: "flex",
+  outline: "none",
   padding: "10px 0",
-  transition: "all 200ms ease",
-  i: {
-    marginRight: 10,
-    fontSize: 16
-  },
+  position: "relative",
+  ...typography.normal.regular,
   "&[data-disabled='true']": {
     cursor: "not-allowed",
     pointerEvents: "none",
-    i: {
+    [`.${radioIconStyle}`]: {
       color: colors.gray.light
     }
   }
--- stories/option.story.tsx
+++ stories/option.story.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { storiesOf } from "@storybook/react";
 import { Option } from "../src";
 import { withState } from "@dump247/storybook-state";
+import { css } from "emotion";
 
 storiesOf("Option", module).add(
   "Default",
@@ -12,6 +13,23 @@ storiesOf("Option", module).add(
         onChange={() => store.set({ checked: !store.state.checked })}
         value="option"
         label="I am an option"
+        multiSelect
+      />
+      <Option
+        isSelected={store.state.checked}
+        onChange={() => store.set({ checked: !store.state.checked })}
+        value="option"
+        label="I am an option with very long text wrapped"
+        multiSelect
+        labelClassName={css({
+          whiteSpace: "normal"
+        })}
+      />
+      <Option
+        isSelected={store.state.checked}
+        onChange={() => store.set({ checked: !store.state.checked })}
+        value="option"
+        label="I am an option with very long text wrapped"
         multiSelect
       />
     </div>
