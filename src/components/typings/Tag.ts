1d58b605fd1954b5fe11f86c2af3d78675493d52
Satwik Gupta <satwik.cpp@gmail.com>
2018-12-12 13:34:55 +0530

    allow to add JSX.Element in label

--- src/components/typings/Tag.ts
+++ src/components/typings/Tag.ts
@@ -2,7 +2,7 @@ import { colors } from "../../theme";
 import { SetDifference } from "utility-types";
 
 export interface TagProps {
-  label: string | number;
+  label: string | number | JSX.Element;
   color: SetDifference<keyof typeof colors, "white">;
   className?: string;
   onClose?: () => void;
