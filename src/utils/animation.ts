30ecca9fd34fb80e5bca6dd074c227c735bb1bc0
ritz078 <rkritesh078@gmail.com>
2018-12-21 12:17:27 +0530

    fixes

--- src/utils/animation.ts
+++ src/utils/animation.ts
@@ -5,7 +5,7 @@ export const animationConfig = {
   enter: { opacity: 1, transform: "scale(1)" },
   leave: { opacity: 0, transform: "scale(0.95)", pointerEvents: "none" },
   config: ((_a: string, motion: State) =>
-    motion === "leave" ? { duration: 50 } : config.stiff) as (
+    motion === "leave" ? { duration: 0.1 } : config.stiff) as (
     key: string
   ) => SpringConfig
 };
