fadcda9bf8a19acf9a89d63efa4bfd7a7f7a4317
ritz078 <rkritesh078@gmail.com>
2018-12-26 12:14:18 +0530

    fix(animation): decrease animation duration

--- src/utils/animation.ts
+++ src/utils/animation.ts
@@ -1,11 +1,13 @@
-import { SpringConfig, State, config } from "react-spring";
+import { SpringConfig, State } from "react-spring";
 
 export const animationConfig = {
   from: { opacity: 0, transform: "scale(0.95)" },
   enter: { opacity: 1, transform: "scale(1)" },
   leave: { opacity: 0, transform: "scale(0.95)", pointerEvents: "none" },
   config: ((_a: string, motion: State) =>
-    motion === "leave" ? { duration: 0.1 } : config.stiff) as (
-    key: string
-  ) => SpringConfig
+    motion === "leave"
+      ? { duration: 0.1 }
+      : {
+          duration: 200
+        }) as (key: string) => SpringConfig
 };
