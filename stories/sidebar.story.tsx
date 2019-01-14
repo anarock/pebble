6eefdab20ce476685f667c1c83089afeccda5e73
Aziz Khambati <aziz.h.khambati@gmail.com>
2019-01-14 13:41:22 +0530

    Reset Sidebar story

--- stories/sidebar.story.tsx
+++ stories/sidebar.story.tsx
@@ -12,52 +12,21 @@ const wrapperStyle = css({
   backgroundColor: "white"
 });
 
-storiesOf("Components/SideBar", module)
-  .add(
-    "simple",
-    withState({ value: false })(({ store }) => (
-      <React.Fragment>
-        <Button onClick={() => store.set({ value: !store.state.value })}>
-          Click Me
-        </Button>
-        <SideBar
-          onClose={() => store.set({ value: false })}
-          width={number("Width", 400)}
-          isOpen={store.state.value}
-          closeOnOutsideClick={boolean("closeOnOutsideClick", true)}
-        >
-          <div className={wrapperStyle} onClick={() => {}} />
-        </SideBar>
-      </React.Fragment>
-    ))
-  )
-  .add(
-    "multi",
-    withState({ sideBarOpen: null as string | null })(({ store }) => (
-      <>
-        <Button onClick={() => store.set({ sideBarOpen: "A" })}>
-          Click Me
-        </Button>
-        {store.state.sideBarOpen === "A" && (
-          <SideBar
-            isOpen
-            onClose={() => store.set({ sideBarOpen: "B" })}
-            width={number("Width", 400)}
-            closeOnOutsideClick={boolean("closeOnOutsideClickA", true)}
-          >
-            A
-          </SideBar>
-        )}
-        {store.state.sideBarOpen === "B" && (
-          <SideBar
-            isOpen
-            onClose={() => store.set({ sideBarOpen: null })}
-            width={number("Width", 500)}
-            closeOnOutsideClick={boolean("closeOnOutsideClickB", true)}
-          >
-            B
-          </SideBar>
-        )}
-      </>
-    ))
-  );
+storiesOf("Components/SideBar", module).add(
+  "simple",
+  withState({ value: false })(({ store }) => (
+    <React.Fragment>
+      <Button onClick={() => store.set({ value: !store.state.value })}>
+        Click Me
+      </Button>
+      <SideBar
+        onClose={() => store.set({ value: false })}
+        width={number("Width", 400)}
+        isOpen={store.state.value}
+        closeOnOutsideClick={boolean("closeOnOutsideClick", true)}
+      >
+        <div className={wrapperStyle} onClick={() => {}} />
+      </SideBar>
+    </React.Fragment>
+  ))
+);
