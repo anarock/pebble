6ae5cd306da82613c3b4a05f402e36dce639f9f6
Satwik Gupta <satwik.cpp@gmail.com>
2019-03-05 14:04:58 +0530

    fix a date for visual testing

--- stories/calendar.story.tsx
+++ stories/calendar.story.tsx
@@ -5,16 +5,18 @@ import Calendar from "../src/components/Calendar";
 import { action } from "@storybook/addon-actions";
 import { colors } from "../src/theme";
 
+const LAST_DATE_OF_YEAR = new Date(new Date().getFullYear(), 11, 31);
+
 storiesOf("Components/Calendar", module)
   .add("Single Date", () => (
     <Calendar
       onChange={action("change")}
       onApply={action("apply")}
       onClear={action("clear")}
-      maxDate={new Date()}
+      maxDate={LAST_DATE_OF_YEAR}
       tileDots={object("tileDots", [
         {
-          timeStamp: Date.now(),
+          timeStamp: LAST_DATE_OF_YEAR,
           colors: [colors.blue.base, colors.emerald.base, colors.yellow.base]
         }
       ])}
@@ -26,10 +28,10 @@ storiesOf("Components/Calendar", module)
       onChange={action("change")}
       onApply={action("apply")}
       onClear={action("clear")}
-      maxDate={new Date()}
+      maxDate={LAST_DATE_OF_YEAR}
       tileDots={object("tileDots", [
         {
-          timeStamp: Date.now(),
+          timeStamp: LAST_DATE_OF_YEAR,
           colors: [colors.blue.base, colors.emerald.base, colors.yellow.base]
         }
       ])}
