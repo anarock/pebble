b0c3ef3cdef764a86a4f46725659e9bfdeec9182
Satwik Gupta <satwik.cpp@gmail.com>
2019-03-05 14:08:03 +0530

    remove duplicate imports

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -6,4 +6,3 @@ import "@storybook/addon-options/register";
 import "@storybook/addon-backgrounds/register";
 import "@storybook/addon-a11y/register";
 import "@storybook/addon-viewport/register";
-import "@storybook/addon-options/register";
