85caf816cc6f9e0cdd9a6821e1033f8dfcce67a5
Aziz Khambati <aziz.h.khambati@gmail.com>
2019-03-14 17:46:05 +0530

    Fix CommonJS Distribution (#214)
    * Fix CJS Dist
    
    * Revert commonjs config
    
    * Change module to esnext
    
    * Fix UMD build
    
    * Fix UMD Build and add documentation
    
    * Get module and main builds working
    
    * Fix all builds and tests

--- rollup.config.js
+++ rollup.config.js
@@ -9,73 +9,120 @@ import commonjs from "rollup-plugin-commonjs";
 import cleanup from "rollup-plugin-cleanup";
 
 const input = "compiled/index.js";
-const external = ["react", "react-calendar/dist/entry.nostyle"];
+const umdGlobals = {
+  react: "React",
+  "react-dom": "ReactDOM"
+};
+const externals = [
+  "react-calendar/dist/entry.nostyle",
+  ...Object.keys(pkg.peerDependencies),
+  ...Object.keys(pkg.dependencies)
+];
 
-function getPlugins(targets) {
-  return [
-    babel({
-      ...babelConfig,
-      presets: [
-        ...babelConfig.presets.filter(preset => {
-          Array.isArray(preset)
-            ? preset[0] !== "@babel/preset-env"
-            : preset !== "@babel/preset-env";
-        }),
-        ["@babel/preset-env", { targets }]
-      ]
-    }),
-    resolve({
-      extensions: [".js", ".jsx", ".json"]
-    }),
-    commonjs(),
-    cleanup({
-      comments: [/^\*#__PURE__/, /^\*@__PURE__/]
-    }),
-    sourceMaps(),
-    filesize()
-  ];
+function external(id) {
+  if (externals.includes(id)) return true;
+  return /^@babel\/runtime/.test(id);
 }
+
+const plugins = [
+  resolve({
+    extensions: [".js", ".jsx", ".json"]
+  }),
+  commonjs(),
+  cleanup({
+    comments: [/^\*#__PURE__/, /^\*@__PURE__/]
+  }),
+  sourceMaps(),
+  filesize()
+];
 
 export default [
   {
     input,
-    external: ["react", "react-dom"],
+    external,
     output: [
       {
-        file: pkg.unpkg,
-        format: "umd",
-        sourcemap: true,
-        name: "pebble",
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM"
-        }
-      }
-    ],
-    plugins: getPlugins({ node: "6" })
-  },
-  {
-    input,
-    external: external.concat(Object.keys(pkg.dependencies)),
-    output: [
-      {
-        file: pkg.module,
-        format: "es",
+        file: pkg.esnext,
+        format: "esm",
         sourcemap: true
       }
     ],
-    plugins: getPlugins({ node: "10" })
+    plugins: [
+      babel({
+        ...babelConfig,
+        exclude: "node_modules/**",
+        runtimeHelpers: true
+      }),
+      ...plugins
+    ]
   },
   {
     input,
-    external: external.concat(Object.keys(pkg.dependencies)),
+    external,
     output: [
+      {
+        file: pkg.module,
+        format: "esm",
+        sourcemap: true
+      },
       {
         file: pkg.main,
         format: "cjs",
         sourcemap: true
       }
     ],
-    plugins: getPlugins({ node: "6" })
+    plugins: [
+      babel({
+        ...babelConfig,
+        babelrc: false,
+        exclude: "node_modules/**",
+        runtimeHelpers: true,
+        presets: [
+          [
+            "@babel/preset-env",
+            { targets: { chrome: "49", safari: "9", ie: "11" } }
+          ],
+          ...babelConfig.presets.filter(
+            preset =>
+              (Array.isArray(preset) ? preset[0] : preset) !==
+              "@babel/preset-env"
+          )
+        ]
+      }),
+      ...plugins
+    ]
+  },
+  {
+    input,
+    external: Object.keys(umdGlobals),
+    output: [
+      {
+        file: pkg.unpkg,
+        format: "umd",
+        sourcemap: true,
+        name: "pebble",
+        globals: umdGlobals
+      }
+    ],
+    plugins: [
+      babel({
+        ...babelConfig,
+        babelrc: false,
+        exclude: "node_modules/**",
+        runtimeHelpers: true,
+        presets: [
+          [
+            "@babel/preset-env",
+            { targets: { chrome: "49", safari: "9", ie: "11" } }
+          ],
+          ...babelConfig.presets.filter(
+            preset =>
+              (Array.isArray(preset) ? preset[0] : preset) !==
+              "@babel/preset-env"
+          )
+        ]
+      }),
+      ...plugins
+    ]
   }
 ];
