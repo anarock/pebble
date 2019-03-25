f29474d5809b0d3bef3ededc6ae38c41ee81e5c1
Satwik Gupta <satwik.cpp@gmail.com>
2019-03-25 14:53:35 +0530

    Fix date for calendar's visual test (#210)
    * fix a date for visual testing
    
    * remove duplicate imports
    
    * remove maxDate prop and fix a date for timestamp

--- stories/calendar.story.tsx
+++ stories/calendar.story.tsx
@@ -5,16 +5,17 @@ import Calendar from "../src/components/Calendar";
 import { action } from "@storybook/addon-actions";
 import { colors } from "../src/theme";
 
+const LAST_DATE_OF_2018 = new Date(2018, 11, 31);
+
 storiesOf("Components/Calendar", module)
   .add("Single Date", () => (
     <Calendar
       onChange={action("change")}
       onApply={action("apply")}
       onClear={action("clear")}
-      maxDate={new Date()}
       tileDots={object("tileDots", [
         {
-          timeStamp: Date.now(),
+          timeStamp: LAST_DATE_OF_2018,
           colors: [colors.blue.base, colors.emerald.base, colors.yellow.base]
         }
       ])}
@@ -26,10 +27,9 @@ storiesOf("Components/Calendar", module)
       onChange={action("change")}
       onApply={action("apply")}
       onClear={action("clear")}
-      maxDate={new Date()}
       tileDots={object("tileDots", [
         {
-          timeStamp: Date.now(),
+          timeStamp: LAST_DATE_OF_2018,
           colors: [colors.blue.base, colors.emerald.base, colors.yellow.base]
         }
       ])}
