37a25a188714294d7dc11b6acea160eb6abb107c
ritz078 <rkritesh078@gmail.com>
2019-05-30 18:05:25 +0530

    fix(test): remove pebble-shared from paths

--- packages/pebble-web/jest.config.js
+++ packages/pebble-web/jest.config.js
@@ -5,11 +5,8 @@ module.exports = {
   name: pkg.name,
   preset: "ts-jest",
   snapshotSerializers: ["enzyme-to-json/serializer"],
-  testMatch: ["**/?(*.)+(spec|test).(j|t)s?(x)"],
-  setupFiles: [
-    require.resolve("intersection-observer"),
-    "<rootDir>/tests/__setup__/setup.ts"
-  ],
+  testMatch: ["**/?(*.)+(spec|test).(t)s?(x)"],
+  setupFiles: ["intersection-observer", "<rootDir>/tests/__setup__/setup.ts"],
   setupFilesAfterEnv: ["<rootDir>/tests/__setup__/setupFramework.ts"],
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
