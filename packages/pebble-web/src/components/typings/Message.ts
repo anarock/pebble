752a2a99a767c9614fe0649f11febee1a16a8406
ritz078 <rkritesh078@gmail.com>
2019-06-24 17:55:04 +0530

    fix:

--- packages/pebble-web/src/components/typings/Message.ts
+++ packages/pebble-web/src/components/typings/Message.ts
@@ -1,5 +1,7 @@
+import * as React from "react";
+
 export interface AlertProps {
   intent: "error" | "success";
   className?: string;
-  text: string;
+  text: React.ReactChild;
 }
--- packages/pebble-web/stories/message.story.tsx
+++ packages/pebble-web/stories/message.story.tsx
@@ -3,7 +3,7 @@ import { storiesOf } from "@storybook/react";
 import { select } from "@storybook/addon-knobs";
 import Message from "../src/components/Message";
 
-storiesOf("Components/Message", module).add("test", () => (
+storiesOf("Components/Message", module).add("default", () => (
   <Message
     intent={select("intent", ["success", "error"], "success")}
     text="You have successfully submitted your entry."
