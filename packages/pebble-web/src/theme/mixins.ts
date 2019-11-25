839d3a198414beaad3b2f6f541ac2abbb02d542f
Aziz Khambati <aziz.h.khambati@gmail.com>
2019-11-25 16:17:44 +0530

    const mixins

--- packages/pebble-web/src/theme/mixins.ts
+++ packages/pebble-web/src/theme/mixins.ts
@@ -1,35 +1,31 @@
-import * as React from "react";
-import { CSSObject } from "@emotion/css";
-
-type Styles = CSSObject | React.CSSProperties;
-
-export const textEllipsis: React.CSSProperties = {
+export const textEllipsis = {
   overflowX: "hidden",
   textOverflow: "ellipsis",
   whiteSpace: "nowrap"
-};
+} as const;
 
-export const flexRow: React.CSSProperties = {
+export const flexRow = {
   display: "flex",
   flexDirection: "row"
-};
+} as const;
 
-export const flexSpaceBetween: React.CSSProperties = {
+export const flexSpaceBetween = {
   ...flexRow,
   justifyContent: "space-between",
   alignContent: "initial"
-};
+} as const;
 
-export const flexMiddleAlign: Styles = {
+export const flexMiddleAlign = {
   display: "flex",
   alignItems: "center",
   justifyContent: "center"
-};
+} as const;
 
-export const getPlaceholderStyle = (color: string): Styles => ({
-  "::-webkit-input-placeholder": { color },
-  "::-moz-placeholder": { color },
-  ":-ms-input-placeholder": { color },
-  ":-moz-placeholder": { color },
-  "::placeholder": { color }
-});
+export const getPlaceholderStyle = (color: string) =>
+  ({
+    "::-webkit-input-placeholder": { color },
+    "::-moz-placeholder": { color },
+    ":-ms-input-placeholder": { color },
+    ":-moz-placeholder": { color },
+    "::placeholder": { color }
+  } as const);
