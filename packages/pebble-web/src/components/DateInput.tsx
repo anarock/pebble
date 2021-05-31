f1409fcd3a2a35d57988f0f519c4c0e6fe6c32ef
ChaitanyaGadodia <chaitanya.gadodia01@gmail.com>
2021-05-31 18:13:40 +0530

    date input

--- packages/pebble-web/src/components/DateInput.tsx
+++ packages/pebble-web/src/components/DateInput.tsx
@@ -13,6 +13,7 @@ import { Rifm } from "rifm";
 import { startOfDay, format } from "date-fns";
 import NativeDateInput from "./NativeDateInput";
 import { UserAgentInfoContext } from "../utils/useragent";
+import { cx } from "emotion";
 
 const noop = () => {};
 
@@ -107,7 +108,7 @@ export default class DateInput extends React.PureComponent<
                   ...(inputProps && inputProps.inputProps),
                   onChange
                 }}
-                className={inputStyle}
+                className={cx(inputStyle, inputProps && inputProps.className)}
               />
             )}
           </Rifm>
