b2f09b48f63de4c99a61c800ffa5b5cb335e95e5
Anusatya Choudhary <59761617+anusatya-choudhary@users.noreply.github.com>
2023-02-20 20:05:15 +0530

    add blue dark (#421)
    * add blue dark
    
    * correct order
    
    * correct order again

--- packages/pebble-shared/src/theme/typings/colors.ts
+++ packages/pebble-shared/src/theme/typings/colors.ts
@@ -17,11 +17,17 @@ interface Light {
   light: string;
 }
 
+interface Blue {
+  dark: string;
+  base: string;
+  light: string;
+}
+
 export interface Colors {
   violet: Dense;
   green: Light;
   yellow: Light;
-  blue: Light;
+  blue: Blue;
   white: White;
   emerald: Dense;
   red: Dense;
