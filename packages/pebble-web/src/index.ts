bf06a1ca3408889d442c3f8f75bc6c371f7e5092
Venkat Sai <venkat.bennabhaktula@anarock.com>
2026-02-16 16:07:41 +0530

    fix import

--- packages/pebble-web/src/index.ts
+++ packages/pebble-web/src/index.ts
@@ -1,6 +1,7 @@
 export * from "./theme";
 export * from "./components";
 export * from "./utils/useragent";
+export * from "./utils/testIds";
 
 import * as utils from "./utils";
 import { colors } from "pebble-shared";
